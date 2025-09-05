8edcfa1ef6a6381b5414b98106e77129b6a08b06
Haseung Ryu <haseung.ryu.ai@gmail.com>
2025-09-05 17:19:29 +0900

    원형 버퍼를 이용하여 SMA 계산 최적화

--- Includes/Indicators/SimpleMovingAverage.hpp
+++ Includes/Indicators/SimpleMovingAverage.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+// 표준 라이브러리
+#include <vector>
+
 // 내부 헤더
 #include "Engines/Indicator.hpp"
 
@@ -11,13 +14,16 @@ class SimpleMovingAverage final : public Indicator {
                                double period);
 
  private:
+  Indicator& source_;
   double double_period_;
   size_t sizet_period_;
 
-  Indicator& source_;
   int count_;
   long double sum_;
   bool can_calculate_;
+
+  vector<double> buffer_;
+  size_t buffer_idx_;
 
   void Initialize() override;
   Numeric<long double> Calculate() override;
